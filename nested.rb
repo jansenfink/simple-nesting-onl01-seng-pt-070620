75da9a959313ed91a6d1c90ab16d6c5ac518d510
Jansen Fink <jansenfink@charter.net>
2020-07-19 00:53:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -84,7 +84,8 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[yukihiro_matsumoto] = {known for: "Ruby", 
+    languages: ["LISP", "C"]}
 end
 
 def changing_alan
@@ -103,8 +104,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
@@ -124,6 +124,6 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     } programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
